4bf2b968d7d489aff34ce64974f95434069f6e8b
Neo Ighodaro <neo@creativitykills.co>
2019-04-09 08:04:28 +0200

    Update AppConstants.swift

--- iosapp/dashboard/AppConstants.swift
+++ iosapp/dashboard/AppConstants.swift
@@ -12,5 +12,5 @@ class AppConstants {
     static let API_URL            = "http://127.0.0.1:8000"
     static let PUSHER_CLUSTER     = "PUSHER_CLUSTER"
     static let PUSHER_KEY         = "PUSHER_KEY"
-    static let PUSHER_INSTANCE_ID = "PUSHER_INSTANCE_ID"
+    static let PUSHER_BEAMS_INSTANCE_ID = "PUSHER_BEAMS_INSTANCE_ID"
 }
